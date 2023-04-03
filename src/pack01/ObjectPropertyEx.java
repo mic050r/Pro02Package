b64bf7e86c156220605d2319da264488d99d72fc
mic050r <s2216@e-mirim.hs.kr>
2023-04-04 08:59:47 +0900

    Object 클래스는 모든 클래스의 아버지 클래스이다

--- src/pack01/ObjectPropertyEx.java
+++ src/pack01/ObjectPropertyEx.java
@@ -1,7 +1,8 @@
 package pack01;
 
 public class ObjectPropertyEx {
-	public static void print(Object obj) {	
+	public static void print(Object obj) {	// Object obj = p --> 형변환
+		// Object 클래스는 모든 클래스의 아버지 클래스이다.
 		System.out.println(obj.getClass().getName());	//클래스 이름
 		System.out.println(obj.hashCode()); 			// 해시코드를 출력함. 클래스의 코드값
 		System.out.println(obj.toString());				// obj객체를 문자열로 출력
