a686238af3e5f8eab607824408f764b44af716ef
mic050r <s2216@e-mirim.hs.kr>
2023-04-04 09:49:54 +0900

    예제 2-4 : Rect 클래스와 equals() 만들기 연습

---
+++ src/pack03/Rect.java
@@ -0,0 +1,20 @@
+package pack03;
+
+class Rect {
+	private int width;
+	private int height;
+
+	public Rect(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public boolean equals(Object obj) {// Object obj = b와 같다
+		Rect p = (Rect) obj;// p는 b객체를 저장한것과 같다.
+		if (width * height == p.width * p.height)
+			// this.width, this.height 는 a객체의 멤버
+			return true;
+		else
+			return false;
+	}
+}
