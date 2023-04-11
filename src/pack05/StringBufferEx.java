e1c3e6a9569a58d32bb566737ec6d8836ab5d6e3
mic050r <s2216@e-mirim.hs.kr>
2023-04-11 09:05:06 +0900

    예제 2-8 : StringBuffer 클래스 메소드 활용

---
+++ src/pack05/StringBufferEx.java
@@ -0,0 +1,26 @@
+package pack05;
+
+public class StringBufferEx {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("This"); // sb = "This"
+		// 시험 sb가 무슨 값인지 잘 알고 있기
+		sb.append(" is pencil"); // sb의 문자열 뒤에 덧붙이기
+		System.out.println(sb); // sb = "This is pencil"
+
+		sb.insert(7, " my"); // sb문자열의 인덱스 7번째(0부터 출발) 위치에 " my" 삽입
+		System.out.println(sb); // sb = "This is my pencil"
+
+		sb.replace(8, 10, " your"); // sb문자열의 8번째부터 10-1번째 문자열을 "your"로 교체
+		System.out.println(sb); // This is your pencil
+
+		sb.delete(8, 13); // sb가 가리키는 8번째 문자열에서 13-1번째까지 문자열 삭제
+		System.out.println(sb); // This is pencil
+
+		sb.setLength(4);
+		// sb의 길이는 "This is pencil"이면 14인데, 4로 설정하면
+		// 문자열 앞 4개만 남겨두고 뒤의 문자열이 삭제 된다.
+		System.out.println(sb); // This
+	}
+
+}
